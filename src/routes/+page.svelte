8280130ef3034b1f7b2934261a81e4eb0f6b9206
JetDev22 <thomas.roelig@gmail.com>
2025-02-04 09:54:11 +0100

    quick fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+  
+  // Load additional
+  import {onMount} from 'svelte';
+
+  // Get current time
+  const utc = new Date().toUTCString();
+
+  // Get current BTC Price in € and $ for header
+  let eurPrice = 0;
+  let usdPrice = 0;
+
+  async function getPriceData(){
+    let url = "https://api.coindesk.com/v1/bpi/currentprice/eur.json";
+    const response = await fetch(url);
+    const data = await response.json();
+    eurPrice = data.bpi.EUR.rate;
+    usdPrice = data.bpi.EUR.rate;
+    localStorage.setItem("eurPrice", eurPrice);
+    localStorage.setItem("usdPrice", usdPrice);
+  };
+  
+  onMount(() => getPriceData());
+</script>
+
+<h1>Let's Create Awesome</h1>
